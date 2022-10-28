98305dc3e83e33a4d57f17e522d4a050a4847968
Alejandro Rojas <kmachappy@pm.me>
2022-10-28 03:19:32 -0700

    checkpoint

--- components/Index/VideoHero.tsx
+++ components/Index/VideoHero.tsx
@@ -8,26 +8,31 @@ const VideoHero = () => {
 
             <div className="relative min-h-[500px] sm:min-h-[600px] bg-hero bg-cover bg-center -z-50 overflow-hidden">
 
-                <div className="absolute  w-full h-full bg-black bg-opacity-60 -z-40">
+                <div className="absolute w-full h-full bg-black bg-opacity-70 -z-40">
 
-                    <div className="absolute  top-2/4  w-auto mx-auto h-full z-50">
-                        <h1 className="text-6xl sm:text-6xl text-white font-custom font-bold text-center">Land Closest To<br /> Hell</h1>
-                        <p className="text-white text-center">Orange County, CA</p>
 
-                        <div className="flex justify-center items-center mt-4">
-                            <button className="bg-main-red text-white px-4 py-2 rounded-md font-custom font-bold">Book Now</button>
-                        </div>
-                    </div>
-
-                    <div className="relative w-full h-full m-auto">
+                    <div className=" w-full h-full flex justify-center items-center -z-30 ">
                         <Image
                             src={"/images/tdh-vector.png"}
                             alt={"banner"}
                             width={350}
                             height={350}
-                            className="m-auto"
+                            className="absolute top-10 m-auto w-[250px] sm:w-[350px]"
+                            quality={100}
                         />
                     </div>
+                    <div className="absolute top-[70%] right-0  left-0 z-20">
+                        <h1 className="text-4xl sm:text-6xl text-white font-custom font-bold text-center">
+                        
+                        <p className=" drop-shadow-2xl">
+                        Land Closest To<br /> Hell
+                        </p>
+                        </h1>
+                        
+
+
+                    </div>
+
                 </div>
             </div>
 
