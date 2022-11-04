f6671775bd81d01bc232c55a31c795ba5ce113a7
Alejandro Rojas <kmachappy@pm.me>
2022-11-03 17:32:32 -0700

    added product list

--- components/Merch/Merch.tsx
+++ components/Merch/Merch.tsx
@@ -10,7 +10,7 @@ const Merch = () => {
         <div className=" relative bg-no-repeat bg-center text-white  bg-hero2 h-full w-full bg-contain z-0">
             <div className="absolute w-full h-full bg-black bg-opacity-60 -z-10"></div>
 
-            <Nav></Nav>
+            {/* <Nav></Nav> */}
 
             <div className="flex justify-center items-center font-custom2 py-4 text-2xl sm:text-5xl">
                 Twisted Dreams Merch
