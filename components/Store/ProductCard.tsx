89a4a7aa9e70f307a29293ba76edf6a02521e1c1
Alejandro Rojas <kmachappy@pm.me>
2022-11-04 00:18:10 -0700

    update on shop

--- components/Store/ProductCard.tsx
+++ components/Store/ProductCard.tsx
@@ -7,7 +7,7 @@ const ProductCard = ({ product }) => {
                 <div className="mb-4 w-full relative overflow-hidden">
                     <Link href={"/"} className="h-full w-full overflow-hidden ">
                         <Image
-                            className="object-fill h-[145px] w-full"
+                            className="object-fill h-[150px] w-full"
                             src={product.image}
                             alt={product.name}
                             width={1000}
