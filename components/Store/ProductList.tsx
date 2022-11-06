2abc0cdfc000e0ebbbe1d6cf1a6bc41ce124c94c
Alejandro Rojas <kmachappy@pm.me>
2022-11-05 21:57:20 -0700

    added shopify api

--- components/Store/ProductList.tsx
+++ components/Store/ProductList.tsx
@@ -1,6 +1,6 @@
 import ProductCard from "./ProductCard";
 const ProductList = ({ products }) => {
-    
+
     return (
         <div className=" p-6 container mx-auto w-full ">
             <div className="text-xl sm:text-2xl font-custom2 font-semibold text-white">
@@ -8,9 +8,9 @@ const ProductList = ({ products }) => {
             </div>
             <div className="my-6 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 md:gap-4">
 
-            {products.map((product) => (
-                <ProductCard product={product} key={product.id} />
-            ))}
+                {products.map((product) => (
+                    <ProductCard product={product} key={product.id} />
+                ))}
             </div>
         </div>
     );
--- pages/store/index.tsx
+++ pages/store/index.tsx
@@ -2,14 +2,14 @@ import Layout from "@components/global/Layout"
 import Store from "@components/Store/Store"
 import { shopifyClient, parseShopifyResponse } from '@lib/shopify'
 
-export default function Merch({products}) {
-  console.log('~~~~~~~', products)
+export default function Index({products}) {
   return (
     <Layout title="- Merch">
       <Store products={products} />
     </Layout>
     )
 }
+
 export const getServerSideProps = async () => {
   const products = await shopifyClient.product.fetchAll()
   
