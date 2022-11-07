84bb4b2942d426fb649be702008efad9ca0184a1
Alejandro Rojas <Appleonama@Priscillas-MacBook-Pro.local>
2022-11-07 11:35:57 -0800

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,23 @@
 import Layout from "@components/global/Layout"
 import Index from "@components/Index/Index"
+import { shopifyClient, parseShopifyResponse } from '@lib/shopify'
 
-
-export default function Home() {
-  
+export default function Home({ cart }) {
+  const {length } = cart.customAttributes.length
+  console.log(length)
+  console.log('this is the cart',cart)
   return (
     <Layout title="- Home">
       <Index />
     </Layout>
     )
 }
+
+export const getServerSideProps = async () => {
+  const cart = await shopifyClient.checkout.create()
+  return {
+    props: {
+      cart: parseShopifyResponse(cart)
+    },
+  };
+};
--- pages/store/products/[productHandle].tsx
+++ pages/store/products/[productHandle].tsx
@@ -10,7 +10,6 @@ export default function ProductPage({ product }) {
   return (
     <Layout title={`- ${handle}`}>
       <ProductLayout images={images} product={product}/>
-
     </Layout>
   )
 }
