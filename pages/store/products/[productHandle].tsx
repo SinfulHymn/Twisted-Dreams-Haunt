4c1a4968f97b0da2c31e5cbd39ca1ce96be1cacf
Alejandro Rojas <kmachappy@pm.me>
2022-11-09 18:17:32 -0800

    fixed useref type error

--- pages/store/products/[productHandle].tsx
+++ pages/store/products/[productHandle].tsx
@@ -4,11 +4,11 @@ import { shopifyClient, parseShopifyResponse } from '@lib/shopify'
 import ProductLayout from "@components/Product/ProductLayout";
 
 
-export default function ProductPage({ product }) {
+export default function ProductPage({ product, cart }) {
   const { title, description, images, variants, handle, id,  } = product;
 
   return (
-    <Layout title={`- ${handle}`}>
+    <Layout cart={cart} title={`- ${handle}`}>
       <ProductLayout images={images} product={product}/>
     </Layout>
   )
@@ -19,9 +19,11 @@ export default function ProductPage({ product }) {
 export const getServerSideProps = async ({ params }) => {
   const { productHandle } = params;
   const product = await shopifyClient.product.fetchByHandle(productHandle);
+  const cart = await shopifyClient.checkout.create()
 
   return {
     props: {
+      cart: parseShopifyResponse(cart),
       product: parseShopifyResponse(product)
     },
   };
