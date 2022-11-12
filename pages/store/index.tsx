e067694951c5a30dd0686f294d4e13ba33a25950
Alejandro Rojas <kmachappy@pm.me>
2022-11-11 21:55:06 -0800

    fixed more bugs

--- pages/store/index.tsx
+++ pages/store/index.tsx
@@ -1,23 +1,20 @@
 import Layout from "@components/global/Layout"
 import Store from "@components/Store/Store"
-import { shopifyClient, parseShopifyResponse } from '@lib/shopify'
-import {getProductsInCollection} from "@lib/shopifyql"
+import { getProductsInCollection } from "@lib/Shopifyql"
 
-export default function Index({products, cart}) {
+export default function Index({ products, cart }) {
   return (
-    <Layout cart={cart} title="- Merch">
-      <Store products={products}  />
+    <Layout title="- Merch">
+      <Store products={products} />
     </Layout>
-    )
+  )
 }
 
 export const getServerSideProps = async () => {
-  const cart = await shopifyClient.checkout.create()
   const products = await getProductsInCollection()
 
   return {
     props: {
-      cart: parseShopifyResponse(cart),
       products: products
     },
   };
