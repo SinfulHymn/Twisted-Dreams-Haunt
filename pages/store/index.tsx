76ffc0421994331e17b63c00cd1a1ca43c6601a5
Alejandro Rojas <kmachappy@pm.me>
2023-04-05 13:43:02 -0700

    testing suspense

--- pages/store/index.tsx
+++ pages/store/index.tsx
@@ -1,12 +1,22 @@
 import Store from "@components/Store/Store";
-
+import { getProductsInCollection } from "@lib/Shopifyql";
 import { Suspense } from "react";
 
-export default function Index() {
+export default function Index({ products }) {
   // console.log(products);
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <Store />
+      <Store products={products} />
     </Suspense>
   );
 }
+
+export const getServerSideProps = async () => {
+  const products = await getProductsInCollection();
+
+  return {
+    props: {
+      products: products,
+    },
+  };
+};
