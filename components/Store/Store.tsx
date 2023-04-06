57e41455b16e3bd3dc3116d683be7fd9887eea1e
Alejandro Rojas <kmachappy@pm.me>
2023-04-06 14:19:20 -0700

    testing

--- components/Store/Store.tsx
+++ components/Store/Store.tsx
@@ -1,12 +1,30 @@
 import React from "react";
 import ProductList from "./ProductList";
 import Banner from "@components/global/Banner";
+import { useState, useEffect } from "react";
+import { getProductsInCollection } from "@lib/Shopifyql";
 
-const Store = ({ products }) => {
+const Store = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [productsData, setProductData] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const products = await getProductsInCollection();
+      setProductData(products);
+      setIsLoading(false);
+    };
+    fetchProducts();
+  }, []);
+
   return (
     <div className="relative z-0 h-full w-full  text-white">
       <Banner title={"Twisted Dreams Store"} />
-      <ProductList products={products} />
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <ProductList products={productsData} />
+      )}
     </div>
   );
 };
--- pages/store/index.tsx
+++ pages/store/index.tsx
@@ -1,27 +1,7 @@
 import Store from "@components/Store/Store";
 import { getProductsInCollection } from "@lib/Shopifyql";
-import { useState, useEffect } from "react";
 
 export default function Index({ products }) {
-  const [isLoading, setIsLoading] = useState(true);
-  const [productsData, setProductData] = useState([]);
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const products = await getProductsInCollection();
-      setProductData(products);
-      setIsLoading(false);
-    };
-    fetchProducts();
-  }, []);
-
-  return (
-    <>
-      {isLoading ? (
-        <div className=" text-3xl font-extrabold text-red-600">Loading...</div>
-      ) : (
-        <Store products={productsData} />
-      )}
-    </>
-  );
+  console.log(products);
+  return <Store products={products} />;
 }
