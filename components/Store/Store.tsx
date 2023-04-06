7deabfe415baf4365ce236d3cabc1ee3f6843c87
Alejandro Rojas <kmachappy@pm.me>
2023-04-06 13:54:48 -0700

    testing different way of loading store with useState()

--- components/Store/Store.tsx
+++ components/Store/Store.tsx
@@ -1,25 +1,31 @@
+import React, { useEffect, useState } from "react";
 import ProductList from "./ProductList";
 import { getProductsInCollection } from "@lib/Shopifyql";
 import Banner from "@components/global/Banner";
 
-const Store = (props) => {
-  const { products } = props;
+const Store = () => {
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const products = await getProductsInCollection();
+      setProducts(products);
+      setIsLoading(false);
+    };
+    fetchProducts();
+  }, []);
+
   return (
     <div className="relative z-0 h-full w-full  text-white">
       <Banner title={"Twisted Dreams Store"} />
-      <ProductList products={products} />
+      {isLoading ? (
+        <div className=" text-3xl font-extrabold text-red-600">Loading...</div>
+      ) : (
+        <ProductList products={products} />
+      )}
     </div>
   );
 };
 
 export default Store;
-
-export const getServerSideProps = async () => {
-  const products = await getProductsInCollection();
-
-  return {
-    props: {
-      products: products,
-    },
-  };
-};
